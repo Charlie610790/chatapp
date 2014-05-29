27afff96b8ad007f5ece78a3ac749ed938764ddb
Charlie Jones <myupstateproperties@gmail.com>
2014-05-29 15:48:24 -0400

    setting up html for chat page, template for chatbox window, css for html windows, and js for pulling the information for the chatbox window

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,15 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+var getChatTemplate = _.template($('.chat-row').text());
+$.getJSON('https://api.github.com/users/Charlie610790/repos?client_id=ae0cd330a00ca9c3c446&client_secret=a9ba76c532a3c0dca1907511ae377bee64532a47').done(function(chats){
+    renderChats(chats);
+});
+
+function renderChats (chats){
+    chats.forEach(function(chat){
+
+        var rendered = getChatTemplate(chat);
+
+        $('.chat-box').append(rendered);
+    });
+}
