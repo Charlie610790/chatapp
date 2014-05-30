f841260eb7b4cb125e1bb66a63e6b0dce8f4f011
Charlie Jones <myupstateproperties@gmail.com>
2014-05-30 15:25:25 -0400

    add if statement on foreach

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,25 @@
 'use strict';
 //sets the var getChatTemplate to add the template named chat-row and append in from the following url
 var getChatTemplate = _.template($('.chat-row').text());
-$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/cchat').done(function(chats){
+$.getJSON('http://tiny-pizza-server.herokuapp.com/collections/cchat2').done(function(chats){
 //run function renderChats listed below on the argument of chats
     renderChats(chats);
 });
 
 function renderChats (chats){
-//
     chats.forEach(function(chat){
-//gets the chattemplate and sets it as the var redered
-        var rendered = getChatTemplate(chat);
-//append the chat box window and puts the pulled data in the window
-        $('.chat-box').append(rendered);
-    });
+        if (chat.hasOwnProperty('user') && chat.hasOwnProperty('message') && chat.hasOwnProperty('time')) { 
+    //gets the chattemplate and sets it as the var redered
+            var rendered = getChatTemplate(chat);
+    //append the chat box window and puts the pulled data in the window
+            $('.chat-box').append(rendered);
+        }
+        console.log(chat);
+    })
 }
+
+$(".chat-box").scrollTop(400);
+
 //reset button that clears the chat window
 $('.reset').click(function() {
 	$('.input').val('');
@@ -35,11 +40,11 @@ $('.submitButton').click(function() {
 //takes the value of the message field and clears it
     $('.input').val('');
 //takes the time and sets it at the var time
-    var time = Date.now();
+    var time = '12:00PM';
 //takes Charlie610790 and sets it as the var user
     var user = 'Charlie610790';
 //creates a new instance of the constructor POST passing the three assigned variables
-    var outgoing = new Post(user, message, time);
+    var outgoingPost = new Post(user, message, time);
 });
 
 function Post (user,message,time) {
@@ -48,9 +53,13 @@ function Post (user,message,time) {
     this.time = time || '';
     this.meta = 'Just Jonesn';
 }
-//sends the data outgoing has to the server
-    // updateChat(outgoing);
 
-function updateChat () {
-    $.post('/collections/charliechat',{user: + this.user , message: + this.message, time : this.time, meta: this.meta});
-}
+//function that actually sends the post data to the .post correctly.
+// function updateChat (outgoingPost) {
+//     $.post('http://tiny-pizza-server.herokuapp.com/collections/cchat',
+//  {
+//     user: this.user, 
+//     message: this.message, 
+//     time : this.time, 
+//     meta: this.meta
+// }
